45753ed95110db857a6241d145bfc1bbcef98c8a
pblinux <pbrojas.linux@gmail.com>
2016-09-13 00:52:21 -0600

    Splash screen and main activity created and finished, new resources and values for activities

---
+++ app/src/main/java/org/transxela/activities/MainActivity.java
@@ -0,0 +1,96 @@
+package org.transxela.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import org.transxela.R;
+import org.transxela.adapters.TabViewAdapter;
+import org.transxela.fragments.FragmentDenuncia;
+import org.transxela.fragments.FragmentHome;
+import org.transxela.fragments.FragmentTrafico;
+
+/**
+ * Created by pblinux on 12/09/16.
+ */
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager tabViewPager;
+    private FloatingActionButton buttonNewDenuncia;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Visual elements asignation
+        toolbar = (Toolbar) findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+        tabViewPager = (ViewPager) findViewById(R.id.mainContent);
+        setupViewPager(tabViewPager);
+        tabLayout = (TabLayout) findViewById(R.id.mainTab);
+        tabLayout.setupWithViewPager(tabViewPager);
+        setupTabIcons();
+        tabViewPager.addOnPageChangeListener(this);
+        buttonNewDenuncia = (FloatingActionButton) findViewById(R.id.buttonNewDenuncia);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        switch (position) {
+            case 0:
+                buttonNewDenuncia.show();
+                break;
+            case 1:
+                buttonNewDenuncia.show();
+                break;
+            case 3:
+                buttonNewDenuncia.hide();
+                break;
+            default:
+                buttonNewDenuncia.hide();
+                break;
+        }
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    private void setupTabIcons() {
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.tab_header, null);
+        tabOne.setText("Inicio");
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.ic_home_white_24dp, 0, 0);
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.tab_header, null);
+        tabTwo.setText("Mis Denuncias");
+        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.ic_report_problem_white_24dp, 0, 0);
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
+        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.tab_header, null);
+        tabThree.setText("Trafico");
+        tabThree.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.ic_traffic_white_24dp, 0, 0);
+        tabLayout.getTabAt(2).setCustomView(tabThree);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        TabViewAdapter tabAdapter = new TabViewAdapter(getSupportFragmentManager());
+        tabAdapter.addFragment(new FragmentHome(), "Inicio");
+        tabAdapter.addFragment(new FragmentDenuncia(), "Mis Denuncias");
+        tabAdapter.addFragment(new FragmentTrafico(), "Trafico");
+        viewPager.setAdapter(tabAdapter);
+    }
+
+}
