4705fa2d393fc24ba87cdef80d39ecb650e04cfb
Pablo Bautista <pablo@pixela.com.gt>
2016-10-23 23:14:43 -0600

    Dialog bottom for set location

--- app/src/main/java/org/transxela/activities/SetLocationActivity.java
+++ app/src/main/java/org/transxela/activities/SetLocationActivity.java
@@ -2,9 +2,14 @@ package org.transxela.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,14 +27,25 @@ import org.transxela.R;
  * Created by pblinux on 19/10/16.
  */
 
-public class SetLocationActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class SetLocationActivity extends AppCompatActivity implements OnMapReadyCallback, Button.OnClickListener {
 
     protected MapView mMapView;
     private LatLng coordenadas;
+    private View bottomSheet;
+    private BottomSheetBehavior bottomSheetBehavior;
+    private Button setLocation, cancelLocation;
+    private GoogleMap map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setlocation);
+        bottomSheet = findViewById(R.id.bottom_sheet);
+        setLocation = (Button) bottomSheet.findViewById(R.id.bottomAccept);
+        cancelLocation = (Button) bottomSheet.findViewById(R.id.bottomNo);
+        setLocation.setOnClickListener(this);
+        cancelLocation.setOnClickListener(this);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
         mMapView = (MapView) findViewById(R.id.mapview);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
@@ -37,7 +53,7 @@ public class SetLocationActivity extends AppCompatActivity implements OnMapReady
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
-        float zoom=14.0f;
+        float zoom = 14.0f;
 
         //LatLngBounds bounds = new LatLngBounds(new LatLng(-15.0, 165.0), new LatLng(15.0, -165.0));
         //LatLngBounds XELA = new LatLngBounds(new LatLng(14.86, -91.56), new LatLng(14.88, -91.46));
@@ -45,12 +61,12 @@ public class SetLocationActivity extends AppCompatActivity implements OnMapReady
         CameraPosition XELA_CAMERA = new CameraPosition.Builder().target(new LatLng(14.844875, -91.523197)).zoom(zoom).bearing(0).tilt(0).build();
         googleMap.setMinZoomPreference(12);
         googleMap.setMaxZoomPreference(16.0f);
-       // googleMap.setLatLngBoundsForCameraTarget(XELA);
+        // googleMap.setLatLngBoundsForCameraTarget(XELA);
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(XELA_CAMERA));
         googleMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
             @Override
             public void onCameraChange(CameraPosition cameraPosition) {
-            float zoomito= cameraPosition.zoom;
+                float zoomito = cameraPosition.zoom;
                 LatLngBounds XELA = new LatLngBounds(new LatLng(14.76, -91.56), new LatLng(14.96, -91.46));
                 googleMap.setLatLngBoundsForCameraTarget(XELA);
             }
@@ -59,20 +75,27 @@ public class SetLocationActivity extends AppCompatActivity implements OnMapReady
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
+                map = googleMap;
                 googleMap.clear();
                 googleMap.addMarker(new MarkerOptions().position(latLng));
-                coordenadas=latLng;
+                coordenadas = latLng;
                 //Log.d("latitud",""+coordenadas.latitude);
                 //Log.d("longitud",""+coordenadas.longitude);
-                Intent intent= new Intent();
-                intent.putExtra("Latitud",coordenadas.latitude);
-                intent.putExtra("Longitud",coordenadas.longitude);
-                setResult(RESULT_OK, intent);
-                finish();
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
             }
         });
     }
+
+    /*
+    * Intent intent= new Intent();
+                intent.putExtra("Latitud",coordenadas.latitude);
+                intent.putExtra("Longitud",coordenadas.longitude);
+                setResult(RESULT_OK, intent);
+                finish();
+    *
+    *
+    * */
 
     @Override
     protected void onResume() {
@@ -118,7 +141,30 @@ public class SetLocationActivity extends AppCompatActivity implements OnMapReady
         }
     }
 
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bottomAccept:
+                Intent intent= new Intent();
+                intent.putExtra("Latitud",coordenadas.latitude);
+                intent.putExtra("Longitud",coordenadas.longitude);
+                setResult(RESULT_OK, intent);
+                finish();
+                return;
+            case R.id.bottomNo:
+                map.clear();
+                bottomSheet.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                    }
+                });
+        }
+    }
+
     public LatLng getCoordenadas() {
         return coordenadas;
     }
+
 }
--- app/src/main/java/org/transxela/fragments/FragmentHome.java
+++ app/src/main/java/org/transxela/fragments/FragmentHome.java
@@ -1,17 +1,22 @@
 package org.transxela.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.transxela.R;
+import org.transxela.utils.FontManager;
 
 /**
  * Created by pblinux on 13/09/16.
  */
 public class FragmentHome extends Fragment {
+
 
     public FragmentHome() {
         // Required empty public constructor
@@ -28,4 +33,9 @@ public class FragmentHome extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }
